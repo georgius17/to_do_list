476b696c83bc40f7d3fddce341609294cf3e8396
Jiří Schejbal <jirkaschej@seznam.cz>
2020-03-10 16:37:11 +0100

    edit tasks

--- src/storage.js
+++ src/storage.js
@@ -8,8 +8,8 @@ const storage = (()=>{
     function populateStorage(projects){
         if (projects.length == 0){
             let task1 = factories.taskFactory('Some task', 'do some task', '2020-02-14', 'High');
-            let task2 = factories.taskFactory('Clean my table', 'clean that shit','2020-04-11','Normal');
-            let task3 = factories.taskFactory('Take a pill', 'to feel gooood','2020-03-15','High');
+            let task2 = factories.taskFactory('Clean my table', 'theres a lot of dust','2020-04-11','Normal');
+            let task3 = factories.taskFactory('Manage my books', 'from oldest to newest','2020-03-15','High');
             let project1 = factories.projectFactory('Home',task1);
             let project2 = factories.projectFactory('School',task2,task3);
             projects.push(project1);
